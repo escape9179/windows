756a0936c2e785c0da491934301b095194b0997b
Tre Logan <tr3.logan@gmail.com>
2021-07-16 13:43:04 -0400

    Doing something with MessageBox Result

--- Windows/Main.cpp
+++ Windows/Main.cpp
@@ -1,6 +1,15 @@
 #include <Windows.h>
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR szCmdLine, int iCmdshow) {
-	MessageBox(NULL, TEXT("Hello Windows 98!"), TEXT("HelloMsg"), MB_YESNOCANCEL | MB_DEFBUTTON2 | MB_ICONINFORMATION);
+	int result = MessageBox(NULL, TEXT("Hello Windows 98!"), TEXT("HelloMsg"), MB_YESNOCANCEL | MB_ICONERROR);
+	if (result == IDYES) {
+		MessageBox(NULL, TEXT("You clicked 'Yes'."), TEXT("Result"), MB_OK);
+	} else if (result == IDNO) {
+		MessageBox(NULL, TEXT("You clicked 'No'."), TEXT("Result"), MB_OK);
+	} else if (result == IDCANCEL) {
+		MessageBox(NULL, TEXT("You clicked 'Cancel'."), TEXT("Result"), MB_OK);
+	} else {
+		MessageBox(NULL, TEXT("You clicked on a non-existing button?"), TEXT("Result"), MB_OK);
+	}
 	return 0;
 }
