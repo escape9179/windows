75cd1cb03126ebdb058a8ab9c8e36fe6b9dd86a3
Tre Logan <tr3.logan@gmail.com>
2021-07-16 13:32:48 -0400

    Combining MessageBox constants

--- Windows/Main.cpp
+++ Windows/Main.cpp
@@ -1,6 +1,6 @@
 #include <Windows.h>
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR szCmdLine, int iCmdshow) {
-	MessageBox(NULL, TEXT("Hello Windows 98!"), TEXT("HelloMsg"), 0);
+	MessageBox(NULL, TEXT("Hello Windows 98!"), TEXT("HelloMsg"), MB_YESNOCANCEL | MB_DEFBUTTON2 | MB_ICONINFORMATION);
 	return 0;
 }
