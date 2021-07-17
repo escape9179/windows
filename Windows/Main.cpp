9078730dd07fd57219b69f7dd80deefb5b9b05bf
Tre Logan <tr3.logan@gmail.com>
2021-07-17 02:04:30 -0400

    Creating MessageBoxPrintf function to display resolution

--- Windows/Main.cpp
+++ Windows/Main.cpp
@@ -1,9 +1,27 @@
 #include <cstdio>
 #include <Windows.h>
 
+int CDECL MessageBoxPrintf(TCHAR const *szCaption, TCHAR const *szFormat, ...);
+
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR szCmdLine, int iCmdshow) {
-	wchar_t buffer[100];
-	wsprintf(buffer, L"The result of %i + %i is %i", 5, 3, 5 + 3);
-	MessageBox(NULL, buffer, TEXT("MessageBox"), MB_OK | MB_ICONEXCLAMATION);
+	int cxScreen, cyScreen;
+	cxScreen = GetSystemMetrics(SM_CXSCREEN);
+	cyScreen = GetSystemMetrics(SM_CYSCREEN);
+	MessageBoxPrintf(TEXT("Screen Size"), TEXT("The screen size is %i pixels wide by %i pixels high."), cxScreen, cyScreen);
 	return 0;
+}
+
+int CDECL MessageBoxPrintf(TCHAR const *szCaption, TCHAR const *szFormat, ...) {
+	va_list args;
+
+	// args = (char *) &szFormat + sizeof(szFormat)
+	va_start(args, szFormat);
+	wchar_t buffer[128];
+
+	// Last argument points to argument list
+	_vsnwprintf_s(buffer, sizeof(buffer) / sizeof(wchar_t), szFormat, args);
+
+	// Zeroes out args for no good reason
+	va_end(args);
+	return MessageBox(NULL, buffer, szCaption, MB_OK | MB_ICONEXCLAMATION);
 }
