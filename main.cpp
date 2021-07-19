f037fbdf02166879ed5967cba87304ec1a81e88b
Tre Logan <tr3.logan@gmail.com>
2021-07-19 13:26:20 -0400

    Re-write ShowWindow v2

--- main.cpp
+++ main.cpp
@@ -1,10 +1,9 @@
 #include <Windows.h>
 
-constexpr LPTSTR WINDOW_CLASS_NAME = "Hello World Application";
+LRESULT CALLBACK windowProcedure(HWND windowHandle, UINT messageId, WPARAM wParam, LPARAM lParam);
 
-LRESULT CALLBACK windowProcedure(HWND windowHandle, UINT messageCode, WPARAM wParam, LPARAM lParam);
-
-int WINAPI WinMain(HINSTANCE instance, HINSTANCE previousInstance, LPSTR commandLine, int windowShowMode) {
+int WINAPI WinMain(HINSTANCE instance, HINSTANCE previousInstance, LPSTR commandLine, int showCommand) {
+    LPSTR appName = "Application Name";
     WNDCLASS windowClass;
     windowClass.style = CS_HREDRAW | CS_VREDRAW;
     windowClass.lpfnWndProc = windowProcedure;
@@ -15,17 +14,28 @@ int WINAPI WinMain(HINSTANCE instance, HINSTANCE previousInstance, LPSTR command
     windowClass.hCursor = LoadCursor(instance, IDC_ARROW);
     windowClass.hbrBackground = (HBRUSH) GetStockObject(WHITE_BRUSH);
     windowClass.lpszMenuName = NULL;
-    windowClass.lpszClassName = WINDOW_CLASS_NAME;
+    windowClass.lpszClassName = appName;
 
     if (!RegisterClass(&windowClass)) {
-        MessageBox(NULL, TEXT("There was an error registering the window class"), TEXT("Error"), MB_OK | MB_ICONERROR);
+        MessageBox(NULL, TEXT("Error registering window class"), TEXT("Error"), MB_OK | MB_ICONERROR);
         return 0;
     }
 
-    HWND windowHandle;
-    windowHandle = CreateWindow(WINDOW_CLASS_NAME, TEXT(WINDOW_CLASS_NAME), WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, NULL, NULL, instance, NULL);
+    HWND windowHandle = CreateWindow(
+            appName,
+            TEXT("This is a window name"),
+            WS_OVERLAPPEDWINDOW,
+            CW_USEDEFAULT,
+            CW_USEDEFAULT,
+            CW_USEDEFAULT,
+            CW_USEDEFAULT,
+            NULL,
+            NULL,
+            instance,
+            NULL
+    );
 
-    ShowWindow(windowHandle, windowShowMode);
+    ShowWindow(windowHandle, showCommand);
     UpdateWindow(windowHandle);
 
     MSG message;
@@ -37,26 +47,24 @@ int WINAPI WinMain(HINSTANCE instance, HINSTANCE previousInstance, LPSTR command
     return message.wParam;
 }
 
-LRESULT CALLBACK windowProcedure(HWND windowHandle, UINT messageCode, WPARAM wParam, LPARAM lParam) {
+LRESULT CALLBACK windowProcedure(HWND windowHandle, UINT messageId, WPARAM wParam, LPARAM lParam) {
     HDC deviceContext;
     PAINTSTRUCT paintStruct;
-    RECT rectangle;
-    switch (messageCode) {
+    RECT clientRect;
+    switch (messageId) {
         case WM_CREATE:
             // Do nothing
             return 0;
         case WM_PAINT:
             deviceContext = BeginPaint(windowHandle, &paintStruct);
-            GetClientRect(windowHandle, &rectangle);
-            DrawText(deviceContext, TEXT("Hello, World!"), -1, &rectangle, DT_SINGLELINE | DT_CENTER | DT_VCENTER);
+            GetClientRect(windowHandle, &clientRect);
+            DrawText(deviceContext, TEXT("This is in the center of the screen"), -1, &clientRect, DT_SINGLELINE | DT_CENTER | DT_VCENTER);
             EndPaint(windowHandle, &paintStruct);
             return 0;
         case WM_DESTROY:
             PostQuitMessage(0);
             return 0;
         default:
-            //TODO Implement default case for windowProcedure
-            break;
+            return DefWindowProc(windowHandle, messageId, wParam, lParam);
     }
-    return DefWindowProc(windowHandle, messageCode, wParam, lParam);
 }
