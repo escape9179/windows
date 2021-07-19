8dc7b1568128aa72a579ce23b21adf94be34ddd3
Tre Logan <tr3.logan@gmail.com>
2021-07-19 11:54:30 -0400

    Re-write ShowWindow v1

--- main.cpp
+++ main.cpp
@@ -1,82 +1,62 @@
-#include <windows.h>
+#include <Windows.h>
 
-LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam);
+constexpr LPTSTR WINDOW_CLASS_NAME = "Hello World Application";
 
-void initializeWindowClass(HINSTANCE &instance, WNDCLASS &windowClass, LPCTSTR windowClassName);
+LRESULT CALLBACK windowProcedure(HWND windowHandle, UINT messageCode, WPARAM wParam, LPARAM lParam);
 
-int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR szCmdLine, int iCmdShow) {
-    static TCHAR szAppName[] = "HelloWin";
-    HWND hwnd;
-    MSG msg;
-    WNDCLASS wndclass;
+int WINAPI WinMain(HINSTANCE instance, HINSTANCE previousInstance, LPSTR commandLine, int windowShowMode) {
+    WNDCLASS windowClass;
+    windowClass.style = CS_HREDRAW | CS_VREDRAW;
+    windowClass.lpfnWndProc = windowProcedure;
+    windowClass.cbClsExtra = 0;
+    windowClass.cbWndExtra = 0;
+    windowClass.hInstance = instance;
+    windowClass.hIcon = LoadIcon(instance, IDI_APPLICATION);
+    windowClass.hCursor = LoadCursor(instance, IDC_ARROW);
+    windowClass.hbrBackground = (HBRUSH) GetStockObject(WHITE_BRUSH);
+    windowClass.lpszMenuName = NULL;
+    windowClass.lpszClassName = WINDOW_CLASS_NAME;
 
-    initializeWindowClass(hInstance, wndclass, szAppName);
-
-    // Register window class. If this function fails then it will return 0
-    // indicating an error (on Windows 98 there is no RegisterClassW implemented so it could return 0
-    // for that also.)
-    if (!RegisterClass(&wndclass)) {
-        MessageBox(NULL, TEXT("This program requires Windows NT!"), szAppName, MB_ICONERROR);
+    if (!RegisterClass(&windowClass)) {
+        MessageBox(NULL, TEXT("There was an error registering the window class"), TEXT("Error"), MB_OK | MB_ICONERROR);
         return 0;
     }
 
-    // Creates the window internally in Windows (memory has been allocated for it)
-    hwnd = CreateWindow(szAppName, // window class name
-                        TEXT("The Hello Program"), // window caption
-                        WS_OVERLAPPEDWINDOW, // window style
-                        CW_USEDEFAULT, // initial x position
-                        CW_USEDEFAULT, // initial y position
-                        CW_USEDEFAULT, // initial x size
-                        CW_USEDEFAULT, // initial y size
-                        NULL, // parent window handle
-                        NULL, // window menu handle
-                        hInstance, // program instance handle
-                        NULL); // creation parameters
+    HWND windowHandle;
+    windowHandle = CreateWindow(WINDOW_CLASS_NAME, TEXT(WINDOW_CLASS_NAME), WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, NULL, NULL, instance, NULL);
 
-    // Displays window on the desktop
-    ShowWindow(hwnd, iCmdShow);
+    ShowWindow(windowHandle, windowShowMode);
+    UpdateWindow(windowHandle);
 
-    // Causes window to get painted via WM_PAINT
-    UpdateWindow(hwnd);
-
-    while(GetMessage(&msg, NULL, 0, 0)) {
-        TranslateMessage(&msg);
-        DispatchMessage(&msg);
+    MSG message;
+    while (GetMessage(&message, windowHandle, 0, 0)) {
+        TranslateMessage(&message);
+        DispatchMessage(&message);
     }
-    return msg.wParam;
+
+    return message.wParam;
 }
 
-LRESULT CALLBACK WndProc(HWND hwnd, UINT  message, WPARAM wParam, LPARAM lParam) {
-    HDC hdc;
-    PAINTSTRUCT ps;
-    RECT rect;
-
-    switch(message) {
+LRESULT CALLBACK windowProcedure(HWND windowHandle, UINT messageCode, WPARAM wParam, LPARAM lParam) {
+    HDC deviceContext;
+    PAINTSTRUCT paintStruct;
+    RECT rectangle;
+    switch (messageCode) {
         case WM_CREATE:
-            PlaySound(TEXT("hellowin .wav"), NULL, SND_FILENAME | SND_ASYNC);
+            // Do nothing
             return 0;
         case WM_PAINT:
-            hdc = BeginPaint(hwnd, &ps);
-            GetClientRect(hwnd, &rect);
-            DrawText(hdc, TEXT("Hello, Windows 98!"), -1, &rect, DT_SINGLELINE | DT_CENTER | DT_VCENTER);
-            EndPaint(hwnd, &ps);
+            deviceContext = BeginPaint(windowHandle, &paintStruct);
+            GetClientRect(windowHandle, &rectangle);
+            DrawText(deviceContext, TEXT("Hello, World!"), -1, &rectangle, DT_SINGLELINE | DT_CENTER | DT_VCENTER);
+            EndPaint(windowHandle, &paintStruct);
             return 0;
         case WM_DESTROY:
             PostQuitMessage(0);
             return 0;
+        default:
+            //TODO Implement default case for windowProcedure
+            break;
     }
-    return DefWindowProc(hwnd, message, wParam, lParam);
-}
-
-void initializeWindowClass(HINSTANCE &instance, WNDCLASS &windowClass, LPCTSTR windowClassName) {
-    windowClass.style = CS_VREDRAW | CS_HREDRAW; // The window is to be repainted whenever the vertical or horizontal dimensions of the window change
-    windowClass.lpfnWndProc = WndProc; // Pointer to window procedure function
-    windowClass.cbClsExtra = 0; // Count in bytes of extra space for window class
-    windowClass.cbWndExtra = 0; // Count in bytes of extra space for window
-    windowClass.hInstance = instance; // Handle to program instance passed to WinMain
-    windowClass.hIcon = LoadIcon(instance, IDI_APPLICATION); // Handle to icon
-    windowClass.hCursor = LoadCursor(instance, IDC_APPSTARTING); // Handle to a cursor
-    windowClass.hbrBackground = (HBRUSH) GetStockObject(WHITE_BRUSH); // Handle to a brush
-    windowClass.lpszMenuName = NULL; // The menu name (there is no menu so this is NULL)
-    windowClass.lpszClassName = windowClassName; // The name of the window class (for single window applications it's usually the name of the program)
+    return DefWindowProc(windowHandle, messageCode, wParam, lParam);
 }
