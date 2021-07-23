7421544547ea48037ad1c1b84668deaf6f5433dd
Tre Logan <tr3.logan@gmail.com>
2021-07-23 10:23:00 -0400

    Add Vertical Scrollbar

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <Windows.h>
 #include "SystemMetrics.h"
+#include <cstdio>
 
 LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
 
@@ -45,12 +46,13 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR szCmdLine,
 }
 
 LRESULT CALLBACK WndProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam) {
-    static int cxChar, cxCaps, cyChar;
+    static int cxChar, cxCaps, cyChar, iVscrollPos;
     HDC hdc;
-    int i;
+    int i, strLength;
     PAINTSTRUCT ps;
     TCHAR szBuffer[10];
     TEXTMETRIC tm;
+    USHORT cxClient, cyClient;
 
     switch (message) {
         case WM_CREATE:
@@ -62,23 +64,60 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam) 
             cyChar = tm.tmHeight + tm.tmExternalLeading;
 
             ReleaseDC(hwnd, hdc);
-            return 0;
+
+            SetScrollRange(hwnd, SB_VERT, 0, NUMLINES, FALSE);
+            SetScrollPos(hwnd, SB_VERT, iVscrollPos, TRUE);
+            break;
+        case WM_SIZE:
+            cxClient = LOWORD(lParam);
+            cyClient = HIWORD(lParam);
+            break;
+        case WM_VSCROLL:
+            switch (LOWORD(wParam)) {
+                case SB_LINEUP:
+                    iVscrollPos -= 1;
+                    break;
+                case SB_LINEDOWN:
+                    iVscrollPos += 1;
+                    break;
+                case SB_PAGEUP:
+                    iVscrollPos -= cyClient / cyChar;
+                    break;
+                case SB_PAGEDOWN:
+                    iVscrollPos += cyClient / cyChar;
+                    break;
+                case SB_THUMBPOSITION:
+                    iVscrollPos = HIWORD(wParam);
+                    break;
+                default:
+                    break;
+            }
+            iVscrollPos = max(0, min(iVscrollPos, NUMLINES - 1));
+            if (iVscrollPos != GetScrollPos(hwnd, SB_VERT)) {
+                SetScrollPos(hwnd, SB_VERT, iVscrollPos, TRUE);
+                InvalidateRect(hwnd, NULL, TRUE);
+            }
+            break;
+        case WM_HSCROLL:
+            //TODO Implement horizontal scroll
+            break;
         case WM_PAINT:
             hdc = BeginPaint(hwnd, &ps);
-
             for (i = 0; i < NUMLINES; i++) {
                 TextOut(hdc, 0, cyChar * i, sysmetrics[i].szLabel, lstrlen(sysmetrics[i].szLabel));
                 TextOut(hdc, 22 * cxCaps, cyChar * i, sysmetrics[i].szDesc, lstrlen(sysmetrics[i].szDesc));
                 SetTextAlign(hdc, TA_RIGHT | TA_TOP);
-                TextOut(hdc, 22 * cxCaps + 40 * cxChar, cyChar * i, szBuffer, wsprintf(szBuffer, TEXT("%5d"), GetSystemMetrics(sysmetrics[i].iIndex)));
+                TextOut(hdc, 22 * cxCaps + 40 * cxChar, cyChar * i, szBuffer,
+                        wsprintf(szBuffer, TEXT("%5d"), GetSystemMetrics(sysmetrics[i].iIndex)));
                 SetTextAlign(hdc, TA_LEFT | TA_TOP);
             }
             EndPaint(hwnd, &ps);
-            return 0;
+            break;
         case WM_DESTROY:
             PostQuitMessage(0);
             return 0;
         default:
             return DefWindowProc(hwnd, message, wParam, lParam);
     }
+    return 0;
 }
