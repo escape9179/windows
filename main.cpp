e8ee3b4eb5428c4119a8a3628d5de8d6abb5a55f
escape9179 <tr3.logan@gmail.com>
2022-04-06 14:42:04 -0400

    Hello Win

--- main.cpp
+++ main.cpp
@@ -1,11 +1,9 @@
-#include <Windows.h>
-#include "SystemMetrics.h"
-#include <cstdio>
+#include <windows.h>
 
 LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR szCmdLine, int iCmdShow) {
-    static TCHAR szAppName[] = TEXT("SysMets1");
+    static TCHAR szAppName[] = TEXT("HelloWin");
     HWND hwnd;
     MSG msg;
     WNDCLASS wndclass;
@@ -28,97 +26,46 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR szCmdLine,
 
     hwnd = CreateWindow(
             szAppName,
-            TEXT("Get System Metrics No. 1"),
-            WS_OVERLAPPEDWINDOW | WS_VSCROLL,
-            CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,
-            NULL, NULL, hInstance, NULL
+            TEXT("The Hello Program"),
+            WS_OVERLAPPEDWINDOW,
+            CW_USEDEFAULT,
+            CW_USEDEFAULT,
+            CW_USEDEFAULT,
+            CW_USEDEFAULT,
+            NULL,
+            NULL,
+            hInstance,
+            NULL
     );
 
     ShowWindow(hwnd, iCmdShow);
     UpdateWindow(hwnd);
 
-    while (GetMessage(&msg, NULL, 0, 0)) {
+    while(GetMessage(&msg, NULL, 0, 0)) {
         TranslateMessage(&msg);
         DispatchMessage(&msg);
     }
-
     return msg.wParam;
 }
 
 LRESULT CALLBACK WndProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam) {
-    static int cxChar, cxCaps, cyChar, iVscrollPos;
     HDC hdc;
-    int i, y;
     PAINTSTRUCT ps;
-    TCHAR szBuffer[10];
-    TEXTMETRIC tm;
-    USHORT cxClient, cyClient;
+    RECT rect;
 
     switch (message) {
         case WM_CREATE:
-            hdc = GetDC(hwnd);
-
-            GetTextMetrics(hdc, &tm);
-            cxChar = tm.tmAveCharWidth;
-            cxCaps = (tm.tmPitchAndFamily & 1 ? 3 : 2) * cxChar / 2;
-            cyChar = tm.tmHeight + tm.tmExternalLeading;
-
-            ReleaseDC(hwnd, hdc);
-
-            SetScrollRange(hwnd, SB_VERT, 0, NUMLINES - 1, FALSE);
-            SetScrollPos(hwnd, SB_VERT, iVscrollPos, TRUE);
-            break;
-        case WM_SIZE:
-            cxClient = LOWORD(lParam);
-            cyClient = HIWORD(lParam);
-            break;
-        case WM_VSCROLL:
-            switch (LOWORD(wParam)) {
-                case SB_LINEUP:
-                    iVscrollPos -= 1;
-                    break;
-                case SB_LINEDOWN:
-                    iVscrollPos += 1;
-                    break;
-                case SB_PAGEUP:
-                    iVscrollPos -= cyClient / cyChar;
-                    break;
-                case SB_PAGEDOWN:
-                    iVscrollPos += cyClient / cyChar;
-                    break;
-                case SB_THUMBPOSITION:
-                    iVscrollPos = HIWORD(wParam);
-                    break;
-                default:
-                    break;
-            }
-            iVscrollPos = max(0, min(iVscrollPos, NUMLINES - 1));
-            if (iVscrollPos != GetScrollPos(hwnd, SB_VERT)) {
-                SetScrollPos(hwnd, SB_VERT, iVscrollPos, TRUE);
-                InvalidateRect(hwnd, NULL, TRUE);
-            }
-            break;
-        case WM_HSCROLL:
-            //TODO Implement horizontal scroll
-            break;
+            PlaySound(TEXT("hellowin.wav"), NULL, SND_FILENAME | SND_ASYNC);
+            return 0;
         case WM_PAINT:
             hdc = BeginPaint(hwnd, &ps);
-            for (i = 0; i < NUMLINES; i++) {
-                y = cyChar * (i - iVscrollPos);
-                TextOut(hdc, 0, y, sysmetrics[i].szLabel, lstrlen(sysmetrics[i].szLabel));
-                TextOut(hdc, 22 * cxCaps, y, sysmetrics[i].szDesc, lstrlen(sysmetrics[i].szDesc));
-                SetTextAlign(hdc, TA_RIGHT | TA_TOP);
-                TextOut(hdc, 22 * cxCaps + 40 * cxChar, y, szBuffer,
-                        wsprintf(szBuffer, TEXT("%5d"), GetSystemMetrics(sysmetrics[i].iIndex)));
-                SetTextAlign(hdc, TA_LEFT | TA_TOP);
-            }
+            GetClientRect(hwnd, &rect);
+            DrawText(hdc, TEXT("Hello, Windows 98!"), -1, &rect, DT_SINGLELINE | DT_CENTER | DT_VCENTER);
             EndPaint(hwnd, &ps);
-            break;
+            return 0;
         case WM_DESTROY:
             PostQuitMessage(0);
-            break;
-        default:
-            return DefWindowProc(hwnd, message, wParam, lParam);
+            return 0;
     }
-    return 0;
+    return DefWindowProc(hwnd, message, wParam, lParam);
 }
