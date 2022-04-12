1869684281f0a87e6f744a071c9bb03a930817c3
escape9179 <tr3.logan@gmail.com>
2022-04-11 22:01:46 -0400

    Restarting

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR szCmdLine,
     MSG msg;
     WNDCLASS wndclass;
 
-    wndclass.style = CS_HREDRAW | CS_VREDRAW;
+    wndclass.style = CS_VREDRAW | CS_HREDRAW;
     wndclass.lpfnWndProc = WndProc;
     wndclass.cbClsExtra = 0;
     wndclass.cbWndExtra = 0;
