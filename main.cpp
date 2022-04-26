0364a3b66f816407e8a9e630816f823325f4fbc0
escape9179 <tr3.logan@gmail.com>
2022-04-25 20:38:48 -0400

    Create MessageBox showing some text metric info

--- main.cpp
+++ main.cpp
@@ -2,6 +2,16 @@
 
 LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
 
+void BetterTextOut(HDC hdc, PSTR text, int x, int y);
+
+void MessageBoxPrintf(HWND hwnd, LPCSTR title, LPCSTR format, ...) {
+    va_list arg_list;
+    va_start(arg_list, format);
+    char buffer[1024];
+    wvsprintf(buffer, format, arg_list);
+    MessageBox(hwnd, buffer, title, MB_OK);
+}
+
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR szCmdLine, int iCmdShow) {
     static TCHAR szAppName[] = TEXT("HelloWin");
     HWND hwnd;
@@ -52,10 +62,16 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam) 
     HDC hdc;
     PAINTSTRUCT ps;
     RECT rect;
+    TEXTMETRIC tm;
 
     switch (message) {
         case WM_CREATE:
             PlaySound(TEXT("hellowin.wav"), NULL, SND_FILENAME | SND_ASYNC);
+            hdc = GetDC(hwnd);
+            GetTextMetrics(hdc, &tm);
+            MessageBoxPrintf(hwnd, "Text Metric Info", "Some text metrics... ascent = %li, descent: %li, height: %li, ave width: %li, max width: %li",
+                             tm.tmAscent, tm.tmDescent, tm.tmHeight, tm.tmAveCharWidth, tm.tmMaxCharWidth);
+            ReleaseDC(hwnd, hdc);
             return 0;
         case WM_PAINT:
             hdc = BeginPaint(hwnd, &ps);
@@ -68,4 +84,13 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam) 
             return 0;
     }
     return DefWindowProc(hwnd, message, wParam, lParam);
+}
+
+void BetterTextOut(HDC hdc, PSTR text, int x, int y) {
+    int count = 0;
+    for (int i = 0; ; i++) {
+        if (text[i] != '\0') count++;
+        else break;
+    }
+    TextOut(hdc, x, y, text, sizeof(char) * count);
 }
