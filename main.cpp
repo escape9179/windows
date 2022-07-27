b518607857593e5d6e4eb18ad1234381cd761a08
escape9179 <tr3.logan@gmail.com>
2022-07-27 15:07:04 -0400

    Add sysmets1

--- main.cpp
+++ main.cpp
@@ -1,33 +1,40 @@
 #include <windows.h>
+#include "SystemMetrics.h"
 
 LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR szCmdLine, int iCmdShow) {
-    static TCHAR szAppName[] = TEXT("Bezier");
+    static TCHAR szAppName[] = TEXT("SysMets1");
     HWND hwnd;
     MSG msg;
     WNDCLASS wndclass;
+
     wndclass.style = CS_HREDRAW | CS_VREDRAW;
     wndclass.lpfnWndProc = WndProc;
     wndclass.cbClsExtra = 0;
     wndclass.cbWndExtra = 0;
     wndclass.hInstance = hInstance;
-    wndclass.hIcon = LoadIcon(hInstance, IDI_APPLICATION);
-    wndclass.hCursor = LoadCursor(hInstance, IDC_ARROW);
-    wndclass.hbrBackground = ((HBRUSH) GetStockObject(WHITE_BRUSH));
+    wndclass.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+    wndclass.hCursor = LoadCursor(NULL, IDC_ARROW);
+    wndclass.hbrBackground = (HBRUSH) GetStockObject(WHITE_BRUSH);
     wndclass.lpszMenuName = NULL;
     wndclass.lpszClassName = szAppName;
 
     if (!RegisterClass(&wndclass)) {
-        MessageBox(hwnd, TEXT("Program requires Windows NT!"), szAppName, MB_ICONERROR);
+        MessageBox(NULL, TEXT("This program requires Windows NT!"), szAppName, MB_ICONERROR);
         return 0;
     }
 
-    hwnd = CreateWindow(szAppName, TEXT("Bezier Splines"), WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT,
-                        CW_USEDEFAULT, CW_USEDEFAULT, NULL, NULL, hInstance, NULL);
+    hwnd = CreateWindow(szAppName, TEXT("Get System Metrics No. 1"),
+                        WS_OVERLAPPEDWINDOW,
+                        CW_USEDEFAULT,
+                        CW_USEDEFAULT,
+                        CW_USEDEFAULT,
+                        CW_USEDEFAULT,
+                        NULL, NULL, hInstance, NULL);
+
     ShowWindow(hwnd, iCmdShow);
     UpdateWindow(hwnd);
-
     while (GetMessage(&msg, NULL, 0, 0)) {
         TranslateMessage(&msg);
         DispatchMessage(&msg);
@@ -35,66 +42,41 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR szCmdLine,
     return msg.wParam;
 }
 
-void DrawBezier(HDC hdc, POINT apt[]) {
-    PolyBezier(hdc, apt, 4);
-    MoveToEx(hdc, apt[0].x, apt[0].y, NULL);
-    LineTo(hdc, apt[1].x, apt[1].y);
-    MoveToEx(hdc, apt[2].x, apt[2].y, NULL);
-    LineTo(hdc, apt[3].x, apt[3].y);
-}
-
 LRESULT CALLBACK WndProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam) {
-    static POINT apt[4];
+    static int cxChar, cxCaps, cyChar;
     HDC hdc;
-    int cxClient, cyClient;
+    int i;
     PAINTSTRUCT ps;
+    TCHAR szBuffer[10];
+    TEXTMETRIC tm;
 
     switch (message) {
-        case WM_SIZE:
-            cxClient = LOWORD(lParam);
-            cyClient = HIWORD(lParam);
+        case WM_CREATE:
+            hdc = GetDC(hwnd);
 
-            apt[0].x = cxClient / 4;
-            apt[0].y = cyClient / 2;
+            GetTextMetrics(hdc, &tm);
+            cxChar = tm.tmAveCharWidth;
+            cxCaps = (tm.tmPitchAndFamily & 1 ? 3 : 2) * cxChar / 2;
+            cyChar = tm.tmHeight + tm.tmExternalLeading;
 
-            apt[1].x = cxClient / 2;
-            apt[1].y = cyClient / 4;
-
-            apt[2].x = cxClient / 2;
-            apt[2].y = 3 * cyClient / 4;
-
-            apt[3].x = 3 * cxClient / 4;
-            apt[3].y = cyClient / 2;
-
+            ReleaseDC(hwnd, hdc);
             return 0;
 
-        case WM_LBUTTONDOWN:
-        case WM_RBUTTONDOWN:
-        case WM_MOUSEMOVE:
-            if (wParam & MK_LBUTTON || wParam & MK_RBUTTON) {
-                hdc = GetDC(hwnd);
-                SelectObject(hdc, GetStockObject(WHITE_PEN));
-                DrawBezier(hdc, apt);
-
-                if (wParam & MK_LBUTTON) {
-                    apt[1].x = LOWORD(lParam);
-                    apt[1].y = HIWORD(lParam);
-                }
-                if (wParam & MK_RBUTTON) {
-                    apt[2].x = LOWORD(lParam);
-                    apt[2].y = HIWORD(lParam);
-                }
-                SelectObject(hdc, GetStockObject(BLACK_PEN));
-                DrawBezier(hdc, apt);
-                ReleaseDC(hwnd, hdc);
-            }
-            return 0;
         case WM_PAINT:
-            InvalidateRect(hwnd, NULL, TRUE);
             hdc = BeginPaint(hwnd, &ps);
-            DrawBezier(hdc, apt);
+
+            for (i = 0; i < NUMLINES; i++) {
+                TextOut(hdc, 0, cyChar * i, sysmetrics[i].szLabel, lstrlen(sysmetrics[i].szLabel));
+                TextOut(hdc, 22 * cxCaps, cyChar * i, sysmetrics[i].szLabel, lstrlen(sysmetrics[i].szLabel));
+                SetTextAlign(hdc, TA_RIGHT | TA_TOP);
+                TextOut(hdc, 22 * cxCaps + 40 * cxChar, cyChar * i, szBuffer, wsprintf(szBuffer, TEXT("%5d"),
+                                                                                       GetSystemMetrics(
+                                                                                               sysmetrics[i].iIndex)));
+                SetTextAlign(hdc, TA_LEFT | TA_TOP);
+            }
             EndPaint(hwnd, &ps);
             return 0;
+
         case WM_DESTROY:
             PostQuitMessage(0);
             return 0;
